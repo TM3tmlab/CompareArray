e266ed084c70afd0ab9f6d47574840f00a71421c
TM3tmlab <7882295+TM3tmlab@users.noreply.github.com>
2019-05-04 11:59:52 +0900

    clean up

--- SplitNumlist/SplitNumlist.c
+++ SplitNumlist/SplitNumlist.c
@@ -1,5 +1,4 @@
-﻿// SplitNumlist.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿// SplitNumlist.c
 
 #include <stdio.h>
 #include <assert.h>
@@ -8,12 +7,12 @@
 
 int compareArray(int[], int, int, int[], int, int);
 void splitArray(int[], int[], int[]);
-void showSplitArray(int[] , int[] , int[]);
+void showSplitArray(int[], int[], int[]);
 
 int main(void)
 {
-	int inputNumList[] = {5, 6, 7, -1};
-	int compNumList[] = {1, 2, 3, 0, 2, 3, 4, 0, 5, 0, 5, 6, 7, -1};
+	int inputNumList[] = { 5, 6, 7, -1 };
+	int compNumList[] = { 1, 2, 3, 0, 2, 3, 4, 0, 5, 0, 5, 6, 7, -1 };
 
 	int beginIndex[SPLIT_MAX];
 	int numCountBySplit[SPLIT_MAX];
@@ -45,8 +44,8 @@ int main(void)
 	printf("match at %d\n", matchIndex);
 
 #ifdef _DEBUG
-	int testA[] = {1, 2};
-	int testB[] = {1, 2};
+	int testA[] = { 1, 2 };
+	int testB[] = { 1, 2 };
 	assert(compareArray(testA, 0, 2, testB, 0, 1) == 0);
 	assert(compareArray(testA, 0, 2, testB, 0, 2) == 1);
 	testB[1] = 3;
@@ -110,14 +109,3 @@ void showSplitArray(int compNumList[], int beginIndex[], int numCountBySplit[])
 		printf("\n");
 	}
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
